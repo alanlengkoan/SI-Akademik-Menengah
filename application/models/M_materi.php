83067d09263ca2af74f00a7a9f3e9481185e573b
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-02-02 16:28:52 +0800

    update model materi

--- application/models/M_materi.php
+++ application/models/M_materi.php
@@ -4,7 +4,7 @@ class M_materi extends CI_Model
 {
     public function getAll()
     {
-        $result = $this->db->query("SELECT * FROM materi")->result();
+        $result = $this->db->query("SELECT materi.id_materi, materi.id_guru, materi.judul, materi.tipe, mapel.nama FROM materi LEFT JOIN mapel ON materi.id_mapel = mapel.id_mapel")->result();
         return $result;
     }
 }
