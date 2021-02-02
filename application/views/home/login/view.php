d8b37b7cfa73692ad3353b3128a833591ce22b09
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-02-02 19:19:58 +0800

    update

--- application/views/home/login/view.php
+++ application/views/home/login/view.php
@@ -7,13 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="<?= assets_url() ?>home/images/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/fonts/iconic/css/material-design-iconic-font.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/vendor/animate/animate.css">
-    <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/vendor/animsition/css/animsition.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/vendor/select2/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/vendor/daterangepicker/daterangepicker.css">
     <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/css/util.css">
     <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>home/css/main.css">
 </head>
@@ -30,12 +24,12 @@
                 <br>
                 Login Form
             </span>
-            <div class="wrap-input100 validate-input m-b-20" data-validate="Nama User Wajib diisi">
+            <div class="wrap-input100 validate-input m-b-20" data-validate="Username Wajib diisi!">
                 <?= form_input(array('name' => 'username', 'id' => 'username', 'class' => 'input100', 'placeholder' => 'Username')) ?>
                 <span class="focus-input100"></span>
                 <?= form_error('username', '<div class="error">', '</div>') ?>
             </div>
-            <div class="wrap-input100 validate-input m-b-25" data-validate="Password Wajib Diisi">
+            <div class="wrap-input100 validate-input m-b-25" data-validate="Password Wajib diisi!">
                 <?= form_password(array('name' => 'password', 'id' => 'password', 'class' => 'input100', 'placeholder' => 'Password')) ?>
                 <span class="focus-input100"></span>
                 <?= form_error('password', '<div class="error">', '</div>') ?>
@@ -50,28 +44,15 @@
     <div id="dropDownSelect1"></div>
 
     <script src="<?= assets_url() ?>home/vendor/jquery/jquery-3.2.1.min.js"></script>
-    <script src="<?= assets_url() ?>home/vendor/animsition/js/animsition.min.js"></script>
     <script src="<?= assets_url() ?>home/vendor/bootstrap/js/popper.js"></script>
     <script src="<?= assets_url() ?>home/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?= assets_url() ?>home/vendor/select2/select2.min.js"></script>
-    <script src="<?= assets_url() ?>home/vendor/daterangepicker/moment.min.js"></script>
-    <script src="<?= assets_url() ?>home/vendor/daterangepicker/daterangepicker.js"></script>
-    <script src="<?= assets_url() ?>home/vendor/countdowntime/countdowntime.js"></script>
     <script src="<?= assets_url() ?>home/js/main.js"></script>
     
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js"></script>
 
     <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'UA-23581568-13');
-
-        var untukTambahData = function() {
+        var untukSubmit = function() {
             $('#form-login').parsley();
             $('#form-login').submit(function(e) {
                 e.preventDefault();
