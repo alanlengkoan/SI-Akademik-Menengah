e2cc165c3ca64890ce1f6213662e0215f39caccb
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-02-02 16:29:03 +0800

    update model tugas

--- application/models/M_tugas.php
+++ application/models/M_tugas.php
@@ -4,7 +4,7 @@ class M_tugas extends CI_Model
 {
     public function getAll()
     {
-        $result = $this->db->query("SELECT * FROM tugas")->result();
+        $result = $this->db->query("SELECT tugas.id_tugas, tugas.id_guru, tugas.judul, tugas.tipe, mapel.nama FROM tugas LEFT JOIN mapel ON tugas.id_mapel = mapel.id_mapel")->result();
         return $result;
     }
 }
