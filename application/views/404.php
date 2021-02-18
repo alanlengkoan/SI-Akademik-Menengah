674157635e12d916b419ec1d4962f2a348ebf68a
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-02-18 17:00:00 +0800

    add page not found

---
+++ application/views/404.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>E-learning - AKADEMIK - SMTK Mamasa</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="<?= assets_url() ?>logo.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/owl.carousel.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/owl.theme.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/owl.transitions.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/wave/waves.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/wave/button.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/meanmenu/meanmenu.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/animate.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/normalize.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/scrollbar/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/notika-custom-icon.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/wave/waves.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/dialog/sweetalert2.min.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/dialog/dialog.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/datapicker/datepicker3.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/main.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/style.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/bootstrap-select/bootstrap-select.css">
+    <link rel="stylesheet" href="<?= assets_url() ?>admin/css/responsive.css">
+
+    <script src="<?= assets_url() ?>admin/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <div class="error-page-area">
+        <div class="error-page-wrap">
+            <i class="notika-icon notika-close"></i>
+            <h2>ERROR <span class="counter">404</span></h2>
+            <p>Maaf, tetapi halaman yang Anda cari telah ditemukan. Coba periksa URL apakah ada kesalahan, lalu tekan tombol segarkan di browser Anda atau coba temukan yang lain di aplikasi kami.</p>
+            <a href="<?= base_url() ?>" class="btn">Back</a>
+        </div>
+    </div>
+
+    <script src="<?= assets_url() ?>admin/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/bootstrap.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/wow.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/jquery.scrollUp.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/meanmenu/jquery.meanmenu.js"></script>
+    <script src="<?= assets_url() ?>admin/js/counterup/jquery.counterup.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/counterup/waypoints.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/counterup/counterup-active.js"></script>
+    <script src="<?= assets_url() ?>admin/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/todo/jquery.todo.js"></script>
+    <script src="<?= assets_url() ?>admin/js/plugins.js"></script>
+    <script src="<?= assets_url() ?>admin/js/wave/waves.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/wave/wave-active.js"></script>
+    <script src="<?= assets_url() ?>admin/js/dialog/sweetalert2.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/data-table/jquery.dataTables.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/data-table/data-table-act.js"></script>
+    <script src="<?= assets_url() ?>admin/js/bootstrap-select/bootstrap-select.js"></script>
+    <script src="<?= assets_url() ?>admin/js/main.js"></script>
+    <script src="<?= assets_url() ?>admin/js/datapicker/bootstrap-datepicker.js"></script>
+    <script src="<?= assets_url() ?>admin/js/datapicker/datepicker-active.js"></script>
+    <script src="<?= assets_url() ?>admin/js/icheck/icheck.min.js"></script>
+    <script src="<?= assets_url() ?>admin/js/notify/notify.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</body>
+
+</html>
