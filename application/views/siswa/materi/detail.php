7edcbc24ca5f58141090a2ae3589b2e7acccee61
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-09-04 07:29:12 +0800

    update again

--- application/views/siswa/materi/detail.php
+++ application/views/siswa/materi/detail.php
@@ -29,91 +29,79 @@
 <div class="normal-table-area">
     <div class="container">
         <div class="row">
-            <?php if (date('H:i:s') >= $data->jam_mulai && date('H:i:s') <= $data->jam_selesai) { ?>
-                <?php if ($status == 0) { ?>
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="mybox mg-t-30">
-                            <div class="bsc-tbl">
-                                <button type="button" class="btn btn-success btn-block" id="absen" data-id_materi="<?= $data->id_materi ?>" data-id_siswa="<?= $id_siswa ?>">Absen</button>
-                            </div>
+            <?php if ($status == 0) { ?>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="mybox mg-t-30">
+                        <div class="bsc-tbl">
+                            <button type="button" class="btn btn-success btn-block" id="absen" data-id_materi="<?= $data->id_materi ?>" data-id_siswa="<?= $id_siswa ?>">Absen</button>
                         </div>
                     </div>
-                <?php } else { ?>
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="alert alert-success alert-dismissible" role="alert">
-                            Berhasil, melakukan Absen!
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                        <div class="mybox mg-t-30">
-                            <div class="bsc-tbl">
-                                <h2>Chat Room</h2>
-                                <hr>
-                                <div class="chat-conversation">
-                                    <div class="widgets-chat-scrollbar">
-                                        <!-- begin:: chat -->
-                                        <div id="dom_chat"></div>
-                                        <!-- end:: chat -->
-                                    </div>
-                                    <?php if ($data->status === '1') { ?>
-                                        <div class="chat-widget-input">
-                                            <div class="row">
-                                                <div class="col-sm-12 col-md-12 col-sm-12 col-xs-12 chat-inputbar">
-                                                    <form id="form-add" action="<?= siswa_url() ?>materi/sent_chat" method="POST">
-                                                        <input type="hidden" name="id_materi" value="<?= $data->id_materi ?>" />
-
-                                                        <div class="form-group todo-flex">
-                                                            <div class="nk-int-st">
-                                                                <input type="text" name="pesan" id="pesan" class="form-control chat-input" placeholder="Masukkan Pesan Anda">
-                                                            </div>
-                                                            <div class="chat-send">
-                                                                <button type="submit" id="kirim" class="btn btn-md btn-primary btn-block notika-chat-btn">Kirim</button>
-                                                            </div>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php } ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                        <div class="mybox mg-t-30">
-                            <div class="bsc-tbl">
-                                <h2><?= $halaman ?></h2>
-                                <hr>
-                                <p><?= $data->judul ?></p>
-                                <?php if ($data->jenis_tugas == 'pekerjaan_sekolah') { ?>
-                                    <a <?= (date('H:i:s') >= $data->jam_mulai && date('H:i:s') <= $data->jam_selesai ? 'disabled' : 'href="' . siswa_url() . '/tugas/detail/' . $data->id_tugas . '"') ?> class="btn btn-info btn-block">Tugas Latihan</a>
-                                <?php } ?>
-                                <hr>
-                                <?php if ($data->tipe === 'pdf') { ?>
-                                    <embed style="height: 500px;" src="<?= upload_url('pdf') ?><?= $data->file ?>" type="application/pdf" frameBorder="0" scrolling="auto" height="100%" width="100%"></embed>
-                                <?php } else if ($data->tipe === 'mp4') { ?>
-                                    <video style="max-width: 100%; height: auto;" controls>
-                                        <source src="<?= upload_url('mp4') ?><?= $data->file ?>" type="video/mp4">
-                                    </video>
-                                <?php } ?>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
+                </div>
             <?php } else { ?>
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="alert alert-info alert-dismissible" role="alert">
-                        Materi & Tugas belum dapat di akses, silahkan tunggu jadwal Materi!
+                    <div class="alert alert-success alert-dismissible" role="alert">
+                        Berhasil, melakukan Absen!
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                 </div>
-            <?php } ?>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="mybox mg-t-30">
+                        <div class="bsc-tbl">
+                            <h2>Chat Room</h2>
+                            <hr>
+                            <div class="chat-conversation">
+                                <div class="widgets-chat-scrollbar">
+                                    <!-- begin:: chat -->
+                                    <div id="dom_chat"></div>
+                                    <!-- end:: chat -->
+                                </div>
+                                <?php if ($data->status === '1') { ?>
+                                    <div class="chat-widget-input">
+                                        <div class="row">
+                                            <div class="col-sm-12 col-md-12 col-sm-12 col-xs-12 chat-inputbar">
+                                                <form id="form-add" action="<?= siswa_url() ?>materi/sent_chat" method="POST">
+                                                    <input type="hidden" name="id_materi" value="<?= $data->id_materi ?>" />
 
+                                                    <div class="form-group todo-flex">
+                                                        <div class="nk-int-st">
+                                                            <input type="text" name="pesan" id="pesan" class="form-control chat-input" placeholder="Masukkan Pesan Anda">
+                                                        </div>
+                                                        <div class="chat-send">
+                                                            <button type="submit" id="kirim" class="btn btn-md btn-primary btn-block notika-chat-btn">Kirim</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="mybox mg-t-30">
+                        <div class="bsc-tbl">
+                            <h2><?= $halaman ?></h2>
+                            <hr>
+                            <p><?= $data->judul ?></p>
+                            <?php if ($data->jenis_tugas == 'pekerjaan_sekolah') { ?>
+                                <a <?= (date('H:i:s') >= $data->jam_mulai && date('H:i:s') <= $data->jam_selesai ? 'disabled' : 'href="' . siswa_url() . '/tugas/detail/' . $data->id_tugas . '"') ?> class="btn btn-info btn-block">Tugas Latihan</a>
+                            <?php } ?>
+                            <hr>
+                            <?php if ($data->tipe === 'pdf') { ?>
+                                <embed style="height: 500px;" src="<?= upload_url('pdf') ?><?= $data->file ?>" type="application/pdf" frameBorder="0" scrolling="auto" height="100%" width="100%"></embed>
+                            <?php } else if ($data->tipe === 'mp4') { ?>
+                                <video style="max-width: 100%; height: auto;" controls>
+                                    <source src="<?= upload_url('mp4') ?><?= $data->file ?>" type="video/mp4">
+                                </video>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </div>
